9003d2644cba393d4573f02dad754f62604fa31b
Cooper D'Andilly <151992386+CooperDActor-bytes@users.noreply.github.com>
2024-04-23 17:05:22 +1000

    Create Jason-Dandilly.sh

---
+++ Jason-Dandilly.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+sudo apt update -y
+sudo apt install openssh-server -y
+sudo systemctl start ssh 
+sudo systemctl enable ssh
+
+echo pls check ip 
+
+ip a | grep wlan0
+
+echo i am never gunna give you up
+
+
+echo never gunna let u down 
+
+echo or hurt you
+
+
+
+
+echo ...........
+
+
+echo never gunna make you cry 
+
+echo only when i sayyyy gooood byyyyeeeeee
+
+echo love you
+
+
+echo :)
