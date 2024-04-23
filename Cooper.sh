55cd2f1b923bb384f360d81695245027d2fc2892
Cooper D'Andilly <151992386+CooperDActor-bytes@users.noreply.github.com>
2024-04-23 18:28:34 +1000

    Update Cooper.sh

--- Cooper.sh
+++ Cooper.sh
@@ -2,9 +2,9 @@
 
 
 cd Downloads
-curl https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
+curl -o https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
 
-gdebi chrome-remote-desktop_current_amd64.deb -y 
+gdebi chrome-remote-desktop_current_amd64.deb 
 
 
 DISPLAY= /opt/google/chrome-remote-desktop/start-host --code="4/0AeaYSHAj-bOGLZu5-6N9sh1ymhra3AQivb4JbmusBSv47-hL5mEsOJn2Kkw_Emld4lkcVA" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=$(hostname)
